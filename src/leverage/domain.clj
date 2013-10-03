4530f85990292ed3efae16fc2f89671d565aef20
Robert Stuttaford <robert@cognician.com>
2013-10-03 23:10:59 +0200

    Add demos of data basics.

--- src/leverage/domain.clj
+++ src/leverage/domain.clj
@@ -1,5 +1,60 @@
 (ns leverage.domain)
 
+;; Define and inspect some data
+
+(def sample-data
+  {:cols ["One" "Two" "Three"]
+   :rows [[1 2 3]
+          [4 5 6]
+          [7 8 9]]})
+
+(comment
+  sample-data
+
+  
+
+  (:cols sample-data)
+  (get sample-data :cols)
+
+  
+  
+  (get-in sample-data [:rows 1])
+  
+  
+  )
+
+;; Process some data
+
+(defn row-with-sum
+  [row]
+  (conj row (apply + row)))
+
+(comment
+  (row-with-sum [1 2 3])
+
+
+  )
+
+(defn process-row-sums
+  [rows]
+  (map row-with-sum rows))
+
+(def processed-sample-data
+  (-> sample-data
+      (update-in [:cols] conj "Sum")
+      (update-in [:rows] process-row-sums)))
+
+(comment
+  processed-sample-data
+
+  
+
+  sample-data
+
+  
+  )
+
+
 (defn make-html-table
   [{:keys [cols rows]}]
   [:table.ui.table.segment
@@ -13,8 +68,8 @@
        (for [cell cells]
          [:td cell])])]])
 
-(def sample-data
-  {:cols ["One" "Two" "Three"]
-   :rows [[1 2 3]
-          [4 5 6]
-          [7 8 9]]})
+(comment
+  (make-html-table processed-sample-data)
+
+  
+)
